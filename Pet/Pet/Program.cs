59f748bddbc6b075cacf43132460ce2c9eb68226
Thilaga2001 <thilagavedaraju@gmail.com>
2024-05-29 19:10:25 -0400

    Project Setup and Initial Commit
    Project Setup and Initial Commit

---
+++ Pet/Pet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirtualPetSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Virtual Pet Simulator!");
+        }
+    }
+}
