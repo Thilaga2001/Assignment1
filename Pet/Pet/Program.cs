f9e85fa37b524fba4b8693d72c50e17e05685c76
Thilaga2001 <thilagavedaraju@gmail.com>
2024-05-30 00:03:32 -0400

    Pet Creation Feature
    Pet Creation Feature

--- Pet/Pet/Program.cs
+++ Pet/Pet/Program.cs
@@ -7,6 +7,60 @@ namespace VirtualPetSimulator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Virtual Pet Simulator!");
+
+            // Creating Pet
+            Console.WriteLine("Please choose a type of pet:");
+            Console.WriteLine("1. Cat");
+            Console.WriteLine("2. Dog");
+            Console.WriteLine("3. Rabbit");
+            string petTypeInput = Console.ReadLine();
+
+            string petType;
+            switch (petTypeInput)
+            {
+                case "1":
+                    petType = "Cat";
+                    break;
+                case "2":
+                    petType = "Dog";
+                    break;
+                case "3":
+                    petType = "Rabbit";
+                    break;
+                default:
+                    petType = "Unknown" 
+                    break;
+            }
+
+            Console.WriteLine($"You've chosen a {petType}. What would you like to name your pet?");
+            string petName = Console.ReadLine();
+
+            Pet pet = new Pet(petType, petName); 
+            Console.WriteLine($"Welcome, {pet.Name}! Let's take good care of {pet.Type}.");
         }
+    }
+
+    class Pet
+    {
+        public string Type { get; private set; }
+        public string Name { get; private set; }
+
+        public Pet(string type, string name)
+        {
+            Type = type;
+            Name = name;
+        }
+
+        public void DisplayInfo() 
+        {
+            Console.WriteLine($"Type: {Type}, Name: {Name}");
+        }
+
+        public void SetType(string type) 
+        {
+            Type = type;
+        }
+
+        private string health; 
     }
 }
